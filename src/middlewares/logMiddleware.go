280d5c2cf0a1d39f1526552138fa61c1e8294565
Danilo-DS <danilo_syllva@outlook.com>
2023-10-25 22:55:15 -0300

    add middleware log request

--- src/middlewares/logMiddleware.go
+++ src/middlewares/logMiddleware.go
@@ -1,1 +1,13 @@
 package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+func Logger(nextFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.Host, r.Method, r.RequestURI)
+		nextFunc(w, r)
+	}
+}
